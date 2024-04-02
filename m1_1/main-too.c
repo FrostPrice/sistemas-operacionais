2e0d1de7b61b7cef381792d16053be35db8d6570
Mateus Barbosa <barbosamat16@gmail.com>
2024-04-02 08:57:58 -0300

    Feat: Sync files

--- m1_1/main-too.c
+++ m1_1/main-too.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <time.h> // Adicionando biblioteca time.h para lidar com o tempo
+#include <signal.h>
+#include <time.h> // Usada para lidar com o tempo
 
 // Constantes
 #define NUM_THREADS 3
@@ -17,7 +17,7 @@ typedef struct {
     int id;
     int items_count;
     double weight;
-    pthread_mutex_t mutex;
+    int is_running;
 } Sensor;
 
 // Variaveis Globais (Compartilhadas entre threads)
@@ -27,6 +27,7 @@ int totalItemsCount = 0.0;
 double totalWeight = 0.0;
 Sensor sensors[NUM_THREADS];
 pthread_t sensors_threads[NUM_THREADS];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_attr_t threads_attr;
 int pipe_fd[2];
 // Variáveis globais para controlar o tempo de exibição no display
@@ -35,34 +36,46 @@ time_t current_time;
 
 void* sensorThread(void* param) {
     Sensor* sensor = (Sensor*)param;
-
-    printf("Esteira %d: Iniciando...\n", sensor->id);
-
     char sender_message[100];
+
+    sprintf(sender_message, "Esteira %d: Iniciando...\n", sensor->id);
+
+    pthread_mutex_lock(&(mutex));
+    write(pipe_fd[1], sender_message, sizeof(sender_message));
+    pthread_mutex_unlock(&(mutex));
+
     while (1)
     {
-        pthread_mutex_lock(&(sensor->mutex));
+        while(!sensor->is_running) {
+        }
+
         sensor->items_count++;
-        totalItemsCount++;
         sensor->weight += WEIGHTS[sensor->id];
+
+        pthread_mutex_lock(&(mutex));
+        totalItemsCount++;
         totalWeight += WEIGHTS[sensor->id];
 
         // Verificar se é necessário atualizar o peso total
         if (totalItemsCount % ITEMS_UPDATE_WEIGHT == 0) {
-            sprintf(sender_message, "Quantidade total de Itens: %d, Peso total dos itens: %.2f Kg\n",totalItemsCount, totalWeight);
+            sprintf(sender_message, "Quantidade total de Itens: %d, Peso total dos itens: %.2f Kg\n", totalItemsCount, totalWeight);
             write(pipe_fd[1], sender_message, sizeof(sender_message));
         }
 
         // Verificar se é necessário enviar a contagem para exibição
         current_time = time(NULL);
         if (current_time - last_display_time >= DISPLAY_SECONDS_INTERVAL) {
+            // * Descomente o código abaixo para exibir a contagem de itens e o peso da esteira atual
             // sprintf(sender_message, "Esteira %d: %d itens, peso total: %.2f kg\n", sensor->id, sensor->items_count, sensor->weight); // Exibir a contagem de itens e o peso da esteira atual
+            // write(pipe_fd[1], sender_message, sizeof(sender_message));
+
             sprintf(sender_message, "Contagem total de itens: %d\n", totalItemsCount); // Exibir a contagem total de itens
             write(pipe_fd[1], sender_message, sizeof(sender_message));
+
             last_display_time = current_time;
         }
+        pthread_mutex_unlock(&(mutex));
 
-        pthread_mutex_unlock(&(sensor->mutex));
 
         usleep(INTERVALS[sensor->id] * 1000000); // usleep usa microssegundos
     }
@@ -79,9 +92,36 @@ void displayProcess() {
     }
 }
 
+void* keyboardInput(void* param) {
+    int input_char = 0;
+
+    while(1){
+        input_char = getchar();
+        if (input_char == 80 || input_char == 112)
+        {
+            char sender_message[100];
+            sprintf(sender_message, "WARNING: Contagem interrompida pelo operador, Contagem total de itens: %d\n", totalItemsCount);
+            write(pipe_fd[1], sender_message, sizeof(sender_message));
+            for (int i = 0; i < NUM_THREADS; i++) {
+                sensors[i].is_running = 0;
+            }
+        }
+        if (input_char == 82 || input_char == 114)
+        {
+            char sender_message[100] = "WARNING: Contagem retomada pelo operador\n";
+            write(pipe_fd[1], sender_message, sizeof(sender_message));
+            for (int i = 0; i < NUM_THREADS; i++) {
+                sensors[i].is_running = 1;
+            }
+        }
+    }
+    pthread_exit(0);
+}
+
 void signalHandler(int signal) {
     if (signal == SIGINT) {
-        printf("WARNING: Contagem interrompida pelo operador\n");
+        printf("WARNING: Contagem encerrada pelo operador\n");
+
         for (int i = 0; i < NUM_THREADS; i++) {
             pthread_cancel(sensors_threads[i]);
         }
@@ -92,6 +132,7 @@ void signalHandler(int signal) {
 }
 
 int main() {
+    pthread_mutex_init(&(mutex), NULL);
     pthread_attr_init(&threads_attr);
 
     // Iniciar threads dos sensores e inicializar mutexes
@@ -101,7 +142,6 @@ int main() {
         sensor.id = i;
         sensor.items_count = 0;
         sensor.weight = 0;
-        pthread_mutex_init(&(sensor.mutex), NULL);
         sensors[i] = sensor;
 
         pthread_create(&sensors_threads[i], &threads_attr, sensorThread, (void*)&sensors[i]); 
@@ -116,12 +156,16 @@ int main() {
     pid_t pid = fork();
 
     if (pid == 0) {
-        // Processo filho
+        // Processo filho - Somente recebe mensagens 
         close(pipe_fd[1]);
         displayProcess();
         exit(0);
     } else if (pid > 0) {
-        // Processo pai
+        // Processo pai - Somente envia mensagens
+        // Cria thread do teclado
+        pthread_t keyboardInputThread;
+        pthread_create(&keyboardInputThread, &threads_attr, keyboardInput, NULL);
+        pthread_join(keyboardInputThread, NULL);
         signal(SIGINT, signalHandler);
         for (int i = 0; i < NUM_THREADS; i++) {
             pthread_join(sensors_threads[i], NULL);
@@ -137,10 +181,10 @@ int main() {
     // Aguarda o processo filho encerrar
     wait(NULL);
 
-    // Destruir mutexes
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_mutex_destroy(&(sensors[i].mutex));
-    }
+    // Destruir mutex
+    pthread_mutex_destroy(&(mutex));
 
     return 0;
 }
+
+    // TODO: tentar usar pthread_Cond e pthread_mutex para criar a condição de parada para as threads (ao inves do while(1){})
